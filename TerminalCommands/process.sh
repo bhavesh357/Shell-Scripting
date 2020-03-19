546fb17f5194950f33591d182921ec2d2a2be5e4
Anonymous <Anonymous>
2020-03-19 12:09:51 +0000

    process commit

---
+++ TerminalCommands/process.sh
@@ -0,0 +1,17 @@
+    1  java helloworld.java
+    2  javac helloworld.java
+    3  ls
+    4  touch hello.java
+    5  cd TerminalCommands/
+    6  javac helloworld.java
+    7  java helloworld
+    8  java helloworld
+    9  java helloworld &
+   10  ps -elf | grep java
+   11  ps -elf | grep java | grep -v grep
+   12  jobs
+   13  fg %1
+   14  bg %1
+   15  jobs
+   16  killall java
+   17  history > process.sh
