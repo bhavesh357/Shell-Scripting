6d1f52d3ac2a0acc6b0d7176b696a341179ff360
Anonymous <Anonymous>
2020-03-20 06:09:26 +0000

    second for loop commit

---
+++ TerminalCommands/forloop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+for file in `ls *.txt`;
+
+do 
+  folderName=`echo $file | awk -F. '{print $1}'`;
+  #echo "checking for already existing directory created";
+  if [ -d $folderName ] ;
+  then
+      rm -R $folderName;
+  fi
+  #echo "creating folder ${folderName}";
+  mkdir folderName;
+  #echo copying ${file} to ${folderName};
+  cp $file $folderName;
+done
---
+++ TerminalCommands/loops.sh
@@ -0,0 +1,57 @@
+    1  clear
+    2  touch abc.txt
+    3  touch def.txt
+    4  for file in 'ls *.txt';mv $file terminalCommand
+    5  for file in 'ls *.txt';cp $file terminalCommand
+    6  for file in 'ls *.txt'; do mv $file terminalCommand; done
+    7  for file in 'ls *.txt'; do mv $file terminalCommands; done
+    8  for file in 'ls *.txt'; do mv $file TerminalCommands; done
+    9  ls *.txt
+   10  pwd
+   11  cd TerminalCommands/
+   12  touch abc.txt
+   13  touch def.text
+   14  rm def.text
+   15  ls
+   16  clear
+   17  ls *.txt
+   18  for file in ls *txt; do echo $file;done
+   19  for file in 'ls *txt'; do echo $file;done
+   20  for file in 'ls *.txt'; do echo $file;done
+   21* for file in `ls *.
+   22  for file in `ls *.txt`; do folderName=`echo $file | awk -F. '{print $1}'`; echo $folderName; done
+   23  pwd
+   24  for file in 'ls *.txt'; do folderName=`echo $file | awk -F. '{print $1}'`; mkdir folderName; cp $file $folderName; echo copied $file to $folderName; done
+   25  for file in 'ls *.txt'; do folderName=`echo $file | awk -F. '{print $1}'`; mkdir $folderName; cp $file $folderName; echo copied $file to $folderName; done
+   26  for file in `ls *.txt`; do folderName=`echo $file | awk -F. '{print $1}'`; mkdir $folderName; cp $file $folderName; echo copied $file to $folderName; done
+   27  git add .
+   28  git commit -am "fo loop"
+   29  git push origin master
+   30  touch forloop.sh
+   31  ./forloop.sh
+   32  chmod +x forloop.sh
+   33  ./forloop.sh
+   34  clear
+   35  ./forloop.sh
+   36  ./forloop.sh
+   37  chmod +x forloop.sh
+   38  ./forloop.sh
+   39  ./forloop.sh
+   40  chmod +x forloop.sh
+   41  ./forloop.sh
+   42  ./forloop.sh
+   43  chmod +x forloop.sh
+   44  ./forloop.sh
+   45  chmod +x forloop.sh
+   46  chmod +x forloop.sh
+   47  ./forloop.sh
+   48  chmod +x forloop.sh
+   49  ./forloop.sh
+   50  chmod +x forloop.sh
+   51  ./forloop.sh
+   52  chmod +x forloop.sh
+   53  chmod +x forloop.sh
+   54  ./forloop.sh
+   55  chmod +x forloop.sh
+   56  ./forloop.sh
+   57  history > loops.sh
