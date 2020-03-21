6b1390a3208c8090af8e918506fa748873edd1d1
Bhavesh Kadam <bkadam357@gmail.com>
2020-03-21 15:45:43 +0530

    case second question

--- caseStatement/numberInWord.sh
+++ caseStatement/numberInWord.sh
@@ -3,35 +3,36 @@
 read -p "Enter Number" number 
 word="Enter proper single digit number"
 
-if [ $number -eq 0 ];
-then 
-	word="Zero"
-elif [ $number -eq 1 ];
-then 
-	word="One"
-elif [ $number -eq 2 ];
-then 
-	word="Two"
-elif [ $number -eq 3 ];
-then 
-	word="Three"
-elif [ $number -eq 4 ];
-then 
-	word="Four"
-elif [ $number -eq 5 ];
-then 
-	word="Five"
-elif [ $number -eq 6 ];
-then 
-	word="Six"
-elif [ $number -eq 7 ];
-then 
-	word="Seven"
-elif [ $number -eq 8 ];
-then 
-	word="Eight"
-elif [ $number -eq 9 ];
-then 
-	word="Nine"
-fi
+case $number in 
+		0)
+			word=Zero
+			;;
+		1)
+			word=One
+			;;
+		2)
+			word=Two
+			;;
+		3)
+			word=Three
+			;;
+		4)
+			word=Four
+			;;
+		5)
+			word=Five
+			;;
+		6)
+			word=Six
+			;;
+		7)
+			word=Seven
+			;;
+		8)
+			word=Eight
+			;;
+		9)
+			word=Nine
+			;;
+esac
 echo $word
