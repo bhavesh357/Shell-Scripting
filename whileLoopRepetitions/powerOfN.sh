ce622b0d8f1cf20978945a3c52bff6d437755c00
Bhavesh Kadam <bkadam357@gmail.com>
2020-03-22 12:34:03 +0530

    MODIFIED: powerOfN problem

--- whileLoopRepetitions/powerOfN.sh
+++ whileLoopRepetitions/powerOfN.sh
@@ -4,8 +4,10 @@ echo "Enter Number"
 read num 
 power=1
 count=0
+#counting till power reached
 while [ $count -le $num ]
 do
+	#checking if less than 256
 	if [ $power -le 256 ]
 	then
 		echo $power
