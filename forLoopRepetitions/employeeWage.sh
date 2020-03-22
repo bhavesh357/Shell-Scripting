cdd010172550255886c10f40bd11956891b9a663
Bhavesh Kadam <bkadam357@gmail.com>
2020-03-22 12:01:22 +0530

    lecture problem

---
+++ forLoopRepetitions/employeeWage.sh
@@ -0,0 +1,26 @@
+#!/bon/bash -x
+
+isPartTime=1;
+isFullTime=2;
+totalSalary=0;
+empRatePerHr=20;
+numWorkingDays=20;
+
+for (( day1=0; day<=$numWorkingDays; day++)){
+do
+	empCheck=$((RANDOM%3));
+		case $empCheck in
+			$isFullTime)
+				empHrs=8;
+				;;
+			$isPartTime)
+				empHrs=4;
+				;;
+			*)
+				empHrs=0
+				;;
+		esac
+
+		salary=$(($empHrs*$empRatePerHr));
+		totalSalary=$(($totalSalary+$salary));;
+done
